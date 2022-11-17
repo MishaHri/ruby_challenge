0a1a4219d1591694188d18edfb090c83ae46e436
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-11-17 18:04:06 +0000

    GPA test

--- gpa-test-MishaHri.rb
+++ gpa-test-MishaHri.rb
@@ -1,73 +1,70 @@
 class Calculator
   attr_reader :name, :grades
 
-  #define empty holders
-
-  grades_number = 0
-  grades_count = 0.0
-  gpa = 0.0
-
   def initialize(name, grades)
     @name = name
     @grades = grades
   end
 
-# need to convert the letters on to values
+  # need to convert the letters to numbers
 
-  def change_letter_to_value(letter_value)
+  def change_letter_to_value(letter_grade)
     grade = 0
-    case letter_value
-    when "A"
+    case letter_grade
+    when 'A'
       grade = 4.0
-    when "A-"
+    when 'A-'
       grade = 3.7
-    when "B+"
+    when 'B+'
       grade = 3.3
-    when "B"
+    when 'B'
       grade = 3.0
-    when "B-"
+    when 'B-'
       grade = 2.7
-    when "C+"
+    when 'C+'
       grade = 2.3
-    when "C"
+    when 'C'
       grade = 2.0
-    when "C-"
+    when 'C-'
       grade = 1.7
-    when "D+"
+    when 'D+'
       grade = 1.3
-    when "D"
+    when 'D'
       grade = 1.0
-    when "D-"
+    when 'D-'
       grade = 0.7
-    when "E+"
+    when 'E+'
       grade = 0.5
-    when "E"
+    when 'E'
       grade = 0.2
-    when "E-"
+    when 'E-'
       grade = 0.1
-    when "F"
+    when 'F'
       grade = 0
-    when "U"
+    when 'U'
       grade = -1.0
     else
-      puts "no value"
+      puts 'no value'
     end
     grade
   end
 
-   #define how to calculate a gpa
+  # chgange from letter to value and calculate the sum
 
-  def gpa
-    grades_sum = change_letter_to_value.sum
-    grades_count = change_letter_to_value.length
-    grades_sum / grades_count
-   gpa
+  def grades_sum
+    @grades.map { |grade| change_letter_to_value(grade) }
   end
 
-  #what to print in terminal
+  # finally formula for gpa roudnded to 1 decimal place
+
+  def gpa
+    (grades_sum.sum / @grades.count).round(1)
+  end
+
+  # message to print/interpolation
 
   def announcement
-    puts "#{name} scored an average of #{gpa}"
+    "#{name} scored an average of #{gpa}"
   end
 end
 
@@ -78,9 +75,9 @@ tests = [
   { in: { name: 'Beryl',  grades: ["A", "B", "C"] }, out: { gpa: 3, announcement: "Beryl scored an average of 3.0"  } },
   { in: { name: 'Chris',  grades: ["B-", "C+"] }, out: { gpa: 2.5, announcement: "Chris scored an average of 2.5"  } },
   { in: { name: 'Dan',  grades: ["A", "A-", "B-"] }, out: { gpa: 3.5, announcement: "Dan scored an average of 3.5"  } },
-  { in: { name: 'Emma',  grades: ["A", "B+", "F"] }, out: { gpa: 2.4, announcement: "Beryl scored an average of 2.4"  } },
-  { in: { name: 'Frida',  grades: ["E", "E-"] }, out: { gpa: 0.3, announcement: "Beryl scored an average of 0.3"  } },
-  { in: { name: 'Gary',  grades: ["U", "U", "B+"] }, out: { gpa: 0.4, announcement: "Beryl scored an average of 0.4"  } },
+  { in: { name: 'Emma',  grades: ["A", "B+", "F"] }, out: { gpa: 2.4, announcement: "Emma scored an average of 2.4"  } },
+  { in: { name: 'Frida',  grades: ["E", "E-"] }, out: { gpa: 0.2, announcement: "Frida scored an average of 0.2"  } },
+  { in: { name: 'Gary',  grades: ["U", "U", "B+"] }, out: { gpa: 0.4, announcement: "Gary scored an average of 0.4"  } },
 ]
 
 # how_might_you_do_these = [
